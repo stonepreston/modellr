9464252260363a909ba21e078cda352fc5d2de7c
Stone Preston <stonepreston@protonmail.com>
2021-07-16 13:15:49 -0500

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.less';
-import { Layout, Menu, Tree,  Divider } from 'antd';
+import { Layout, Menu, Tree } from 'antd';
 import {
   SettingOutlined,
-  DownOutlined,
   BranchesOutlined
 } from '@ant-design/icons';
-import { GraphEditor } from './modeling/GraphEditor'
+import { GraphEditor } from './modeling/graph_editor/GraphEditor'
 const { Header, Content, Sider } = Layout;
 const { SubMenu } = Menu;
 
@@ -52,46 +57,59 @@ const App: FC = () => {
   };
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible width={300} collapsedWidth={0} collapsed={collapsed} onCollapse={onCollapse}>
-          <div className="logo">
-            <div className="logo_text">Modellr</div>
-          </div>
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <SubMenu key="sub1" icon={<BranchesOutlined />} title="Models">
-            <DirectoryTree
-              multiple
-              defaultExpandAll
-              treeData={treeData}
-            />
-            </SubMenu>
-            <Menu.Item key="9" icon={<SettingOutlined />}>
-              Settings
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout>
-          <Header style={{ padding: 0 }}>
-            <h1 className="page_header_style">Simple Pipes</h1>
-            <Menu mode="horizontal">
-              <Menu.Item key="model" >
-                Model
-              </Menu.Item>
-              <Menu.Item key="simulation">
-                Simulation
-              </Menu.Item>
-              <Menu.Item key="parameter_estimation">
-                Parameter Estimation
+    <Router>
+      <Layout style={{ minHeight: '100vh' }}>
+          <Sider collapsible width={300} collapsedWidth={0} collapsed={collapsed} onCollapse={onCollapse}>
+            <div className="logo">
+              <div className="logo_text">Modellr</div>
+            </div>
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+              <SubMenu key="sub1" icon={<BranchesOutlined />} title="Models">
+              <DirectoryTree
+                multiple
+                defaultExpandAll
+                treeData={treeData}
+              />
+              </SubMenu>
+              <Menu.Item key="9" icon={<SettingOutlined />}>
+                Settings
               </Menu.Item>
             </Menu>
-          </Header>
-          <Content style={{ margin: '0 16px' }}>
-            <div className="flow" >
-              <GraphEditor />
-            </div>
-          </Content>
+          </Sider>
+          <Layout>
+            <Header style={{ padding: 0 }}>
+              <h1 className="page_header_style">Simple Pipes</h1>
+              <Menu mode="horizontal">
+                <Menu.Item key="model" >
+                  <Link to="/graph_editor">Editor</Link>
+                </Menu.Item>
+                <Menu.Item key="simulation">
+                  <Link to="/simulation">Simulation</Link>
+                </Menu.Item>
+                <Menu.Item key="parameter_estimation">
+                  <Link to="/parameter_estimation">Parameter Estimation</Link>
+                </Menu.Item>
+              </Menu>
+            </Header>
+            <Content style={{ margin: '0 16px' }}>
+            <Switch>
+              <Route path="/graph_editor">
+                <div className="flow" >
+                  <GraphEditor />
+                </div>
+              </Route>
+              <Route path="/simulation">
+                simulation
+              </Route>
+              <Route path="/parameter_estimation">
+                parameter estimation
+              </Route>
+            </Switch>
+              
+            </Content>
+          </Layout>
         </Layout>
-      </Layout>
+      </Router>
   );
 }
 
